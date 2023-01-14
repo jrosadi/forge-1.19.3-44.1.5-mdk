01c3e63f0a8e4ff9c26b1a15ea1fa3408116c539
JR <julian.a.k.rosadi@gmail.com>
2023-01-14 17:55:36 -0600

    added crafting recipes and loot tables

--- src/main/java/net/julian/juliansmod/item/ModItems.java
+++ src/main/java/net/julian/juliansmod/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.julian.juliansmod.item;
 
 import net.julian.juliansmod.JuliansMod;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
