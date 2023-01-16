f81be97188cbd15dd710d2eb723782183c8a8abc
JR <julian.a.k.rosadi@gmail.com>
2023-01-16 12:21:10 -0600

    added explosion on mob hit

--- src/main/java/net/julian/juliansmod/item/custom/TNTSwordItem.java
+++ src/main/java/net/julian/juliansmod/item/custom/TNTSwordItem.java
@@ -1,9 +1,6 @@
 package net.julian.juliansmod.item.custom;
 
-import net.julian.juliansmod.item.ModItems;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
@@ -11,7 +8,6 @@ import net.minecraft.world.level.Level;
 
 public class TNTSwordItem extends SwordItem {
 
-    //Every time that you hit a mob, it causes explosion damage
     //Grants explosion resistance
     //can launch tnt on right click
 
@@ -21,6 +17,8 @@ public class TNTSwordItem extends SwordItem {
 
     @Override
     public boolean hurtEnemy(ItemStack p_43278_, LivingEntity p_43279_, LivingEntity p_43280_) {
+        p_43280_.level.explode(p_43280_, p_43280_.getX(), p_43280_.getY(0.0625D), p_43280_.getZ(), 0, Level.ExplosionInteraction.TNT);
+
         return super.hurtEnemy(p_43278_, p_43279_, p_43280_);
     }
 }
